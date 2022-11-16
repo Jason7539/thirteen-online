6cbb2711843fe14ef0d27bfa861f96aacfd4c333
Jason Nguyen <jnguyen7539@gmail.com>
2022-11-15 21:59:21 -0800

    added eslint and prettier. moved to esmodules

--- src/public/js/client.js
+++ src/public/js/client.js
@@ -1,21 +1,19 @@
-"use strict"
+"use strict";
 
+// eslint-disable-next-line no-undef
 var socket = io();
 
 const MAX_PLAYERS = 4;
 
 socket.on("update-lobby", (lobby) => {
-    console.log("inside update-lobby");
+  console.log("inside update-lobby");
 
-    // TODO: add classes to distinguish the current player's name
-    for (let i = 0; i < MAX_PLAYERS; i++)
-    {
-        document.querySelector("#p"+i).innerHTML = lobby.players[i] ? lobby.players[i].name : '';
-    }
+  // TODO: add classes to distinguish the current player's name
+  for (let i = 0; i < MAX_PLAYERS; i++) {
+    document.querySelector("#p" + i).innerHTML = lobby.players[i]
+      ? lobby.players[i].name
+      : "";
+  }
 });
 
-
-// for hosts and joiners call registerLobbyRoom to update player joing and leaving
-function registerLobbyRoom(lobbyId) {
-    console.log("inside register");
-}
+export { socket };
